36f4e8d4b8cde665a0916ee1138b514a44c685c7
alexanderkuehn125-sudo <alexanderkuehn125@gmail.com>
2026-07-29 15:27:52 +0200

    UI: Added Google Drive link to Impressum placeholder

--- src/components/ImpressumContent.tsx
+++ src/components/ImpressumContent.tsx
@@ -2,7 +2,7 @@ export function ImpressumContent() {
   return (
     <div className="max-w-3xl flex items-center justify-center h-full min-h-[200px] text-[15px] leading-relaxed opacity-90 md:text-base">
       <a 
-        href="#" // TODO: Trage hier den Link zu deinem Impressum ein
+        href="https://drive.google.com/file/d/1h5Ktnw_8Eh1lDnu9MIVd0Ze6BPBUJQf3/view?usp=sharing" 
         target="_blank" 
         rel="noreferrer"
         className="underline underline-offset-4 hover:text-[#C5A059] transition-colors"
